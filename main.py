ae6d1c8560a41c5dd7b86916fde1dfec33c6640a
Zrooney <40244007+Zrooney@users.noreply.github.com>
2018-08-08 11:52:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,7 @@ def imageDownloader(imageURL, recursiveFile = "", saveDest = ""):
             else:
                 out_file = saveDest + os.sep + recursiveFile + os.sep + name
                 checkFile = saveDest + os.sep + recursiveFile
-
-
+                
             if name in os.listdir(checkFile):
                 print(name + " already exists")
             else:
@@ -54,7 +53,7 @@ def imageDownloader(imageURL, recursiveFile = "", saveDest = ""):
         except:
             print("Error Encountered")
 
-rawImageURL = []
+#rawImageURL = []
 imageURL = []
 rawThreadList = []
 threadList = []
@@ -66,52 +65,55 @@ userInput = input("Enter what board you wish to go to, enter just the letter and
 downloadDecision = input("Do you want to download all images into single file, or multiple folders? yes / no")
 saveDest = set_rootdir()
 #page = requests.get("http://boards.4chan.org/"+userInput+"/")
-
-for i in range(1,11):
-    if i ==1:
-        page = requests.get("http://boards.4chan.org/" + userInput + "/")
-        soup = BeautifulSoup(page.content, 'html.parser')
-    else:
-        page = requests.get("http://boards.4chan.org/" + userInput + "/" + str(i))
-        soup = BeautifulSoup(page.content, 'html.parser')
-    #following gets replylinks, and adds only those with link information added
-    for a in soup.find_all('a', class_="replylink"):
-        tempURL = a['href']
-        if (tempURL[-1:].islower() == True):
-            rawThreadList.append(tempURL)
-
-threadList = list(set(rawThreadList))
-print(len(threadList))
-
-# input("Do you want to download all images into single file, or multiple folders? yes / no")
-if (downloadDecision == "yes"):
-    for i in range(len(threadList)):
-        threadDict = (threadList[i].rsplit("/",1))
-        threadFile = saveDest + os.sep + threadDict[1]
-        if (os.path.exists(threadFile) != TRUE):
-            os.mkdir(threadFile)
+while True:
+    for i in range(1,11):
+        if i ==1:
+            page = requests.get("http://boards.4chan.org/" + userInput + "/")
+            soup = BeautifulSoup(page.content, 'html.parser')
         else:
-            print("File already exists.")
-        #print(os.getcwd())
-        #print(threadDict[0])
-        #print(threadDict[1])
-        rawImageURL = imageURLGetter(threadDict[0])
-        imageURL = list(set(rawImageURL))
-        #imageURL = imageURLGetter(threadDict[0]) // commented out to see if i can make a list and remove dupes for faster downloads, this line  does work!
-        imageDownloader(imageURL, threadDict[1],saveDest)
-        imageURL.append(threadDict)
-else:
-    for thread in threadList:
-        tempIndex = thread.rfind('/')
-        massThread = thread[:tempIndex]
-        rawImageURL = imageURLGetter(massThread)
-        imageURL = list(set(rawImageURL))
-        #print(threadList)
-        #print("")
-        #print(imageURL)
-        #print(len(imageURL))
-        imageDownloader(imageURL,"",saveDest)
-print("--- %s seconds ---" % (time.time() - start_time))
+            page = requests.get("http://boards.4chan.org/" + userInput + "/" + str(i))
+            soup = BeautifulSoup(page.content, 'html.parser')
+        #following gets replylinks, and adds only those with link information added
+        for a in soup.find_all('a', class_="replylink"):
+            tempURL = a['href']
+            if (tempURL[-1:].islower() == True):
+                rawThreadList.append(tempURL)
+
+    threadList = list(set(rawThreadList))
+    print(len(threadList))
+
+    # input("Do you want to download all images into single file, or multiple folders? yes / no")
+    if (downloadDecision == "yes"):
+        for i in range(len(threadList)):
+            threadDict = (threadList[i].rsplit("/",1))
+            threadFile = saveDest + os.sep + threadDict[1]
+            if (os.path.exists(threadFile) != TRUE):
+                os.mkdir(threadFile)
+            else:
+                print("File already exists.")
+            #print(os.getcwd())
+            print(threadDict[0])
+            print(threadDict[1])
+            rawImageURL = []
+            rawImageURL = imageURLGetter(threadDict[0])
+            imageURL = []
+            imageURL = list(set(rawImageURL))
+            #imageURL = imageURLGetter(threadDict[0]) // commented out to see if i can make a list and remove dupes for faster downloads, this line  does work!
+            imageDownloader(imageURL, threadDict[1],saveDest)
+            imageURL.append(threadDict)
+    else:
+        for thread in threadList:
+            tempIndex = thread.rfind('/')
+            massThread = thread[:tempIndex]
+            rawImageURL = imageURLGetter(massThread)
+            imageURL = list(set(rawImageURL))
+            #print(threadList)
+            #print("")
+            #print(imageURL)
+            #print(len(imageURL))
+            imageDownloader(imageURL,"",saveDest)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    time.sleep(7200)
 
 # if you change the set_rootdir to be hard coded, putting this true around the main will have it run indefinitley with a cooldown period of the sleep time
 # keep in mind the sleep time is in seconds. Good for having a bot making a 1:1 backup of the chan.
